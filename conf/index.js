2eb8cc651034ff3b3e08f739acdcba456fbd7ee8
BuildTools <unconfigured@null.spigotmc.org>
2021-07-14 13:42:19 +0530

    More JSON

--- conf/index.js
+++ conf/index.js
@@ -9,16 +9,11 @@ function initConf()
 {
     const fs = require('fs')
     const cjson = require('comment-json')
-    // console.log("Initializing Config")
+    const { bot } = require('../eventhandler')
+
     const mainconfig = cjson.parse(fs.readFileSync('./conf/codbot.json').toString())
     this.mainconfig = mainconfig
-
-    // console.log( mainconfig )
-    // console.log( mainconfig.mysqldb )
-    // console.log( mainconfig.mysqldb.host )
     
-    // read all other .ini separately and store them in a smart way somehow.
-    
-    // eventhandler.bot.emit('conf_ready')
-    // console.log("Initializing Config Complete")
+    bot.emit('config_ready')
+    console.log("Initialized: Config")
 }
